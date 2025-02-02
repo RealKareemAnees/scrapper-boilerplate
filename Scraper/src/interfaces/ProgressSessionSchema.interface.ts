5f56345fb55ce7d3b3aa119c3c4e4d671062a2dc
Kareem <141845998+RealKareemAnees@users.noreply.github.com>
2025-02-02 12:53:02 +0200

    add interfaces for AppError, Constants, ProgressSessionSchema, and Scraper

---
+++ Scraper/src/interfaces/ProgressSessionSchema.interface.ts
@@ -0,0 +1,13 @@
+/**
+ * @instance Interface for the progress session object
+ * @property {string} startTime - The time when the session was started
+ * @property {string} lastEdited - The time when the session was last edited
+ * @property {string[]} sessionURLS - The URLs that are part of the session
+ * @property {number} currentURLIndex - The index of the current URL in the sessionURLS array
+ */
+export type ProgressSessionSchema = {
+    startTime: string;
+    lastEdited: string;
+    sessionURLS: string[];
+    currentURLIndex: number;
+};
