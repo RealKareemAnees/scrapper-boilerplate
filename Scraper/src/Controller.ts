1009c098f089be72b35cebe38881268e96a06007
kareem <kareem@email.com>
2025-02-03 17:00:53 +0200

    refactor Controller to inject dependencies for Condnow and Gtahomes services, enhancing testability and maintainability

--- Scraper/src/Controller.ts
+++ Scraper/src/Controller.ts
@@ -1,5 +1,12 @@
 import { ProgressType } from './interfaces/progress.types';
 import { StrategiesNameSpace } from './interfaces/strategies.namespace';
+import { Configs } from '@/providers/configs/Configs';
+import { Database } from '@/providers/database/Database';
+import { ErrorFactory } from '@/providers/error-factory/ErrorFactory';
+import { ErrorHandler } from '@/providers/error-handler/ErrorHandler';
+import { ImageUploader } from '@/providers/image-uploader/ImageUploader';
+import { Logger } from '@/providers/logger/logger';
+import { Progress } from '@/providers/progress/Progress';
 import { CondnowService } from './services/Condonow.service';
 import { GtahomesService } from './services/Gtahomes.service';
 export class Controller {
@@ -10,11 +17,27 @@ export class Controller {
         progressType: ProgressType,
     ): Promise<void> {
         if (strategyName == StrategiesNameSpace.condonow) {
-            const condonowService = new CondnowService();
+            const condonowService = new CondnowService(
+                new Configs(),
+                new Database(),
+                new ErrorFactory(),
+                new ErrorHandler(),
+                new ImageUploader(),
+                new Logger('CondonowService'),
+                new Progress(),
+            );
             await condonowService.scrape(progressType);
         }
         if (strategyName == StrategiesNameSpace.gtahomes) {
-            const gtahomesService = new GtahomesService();
+            const gtahomesService = new GtahomesService(
+                new Configs(),
+                new Database(),
+                new ErrorFactory(),
+                new ErrorHandler(),
+                new ImageUploader(),
+                new Logger('GtahomesService'),
+                new Progress(),
+            );
             await gtahomesService.scrape(progressType);
         }
     }
